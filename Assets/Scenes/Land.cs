e26226c36ffe32b398056ed697ccccd1b148fd9f
Stepan <trofimovjava@gmail.com>
2019-05-12 00:53:17 +0300

    Add Land script

--- Assets/Scenes/Land.cs
+++ Assets/Scenes/Land.cs
@@ -6,44 +6,85 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class Land : MonoBehaviour
 {
-    Mesh mesh;
+	[SerializeField]
+	private int numOfVertices = 51;
+	
+	[SerializeField]
+	private Range randomRangeTop = new Range(7.0f, 8.3f);
+
+	[SerializeField]
+	private Range randomRangeBottom = new Range(1.0f, 2.0f);
+
+	[SerializeField]
+	private bool changed = true;
+
+	System.Random random = new System.Random();
+
+	Mesh mesh;
 
 	List<Vector3> verticesList = new List<Vector3>();
 	List<int> trianglesList = new List<int>();
 
-	private void Start() {
+	private void Start()
+	{
 		mesh = new Mesh();
 		GetComponent<MeshFilter>().mesh = mesh;
+	}
 
-		CreateShape();
-		UpdateShape();
+	private void Update() {
+		if (changed)
+		{
+			CreateShape();
+			UpdateShape();
+			changed = false;
+		}
 	}
 
 	private void CreateShape()
 	{
-		System.Random random = new System.Random();
-		random.Next();
+		GenerateVertices();
+		GenerateTriangles();
+	}
 
+	private void GenerateVertices()
+	{
 		verticesList.Add(new Vector3(0, 0, 0));
-		verticesList.Add(new Vector3(0, 1, 0));
-		verticesList.Add(new Vector3(1, 1, 0));
 
-		trianglesList.Add(0);
-		trianglesList.Add(1);
-		trianglesList.Add(2);
+		for (int id = 0; id < numOfVertices; id++)
+		{
+			if (id % 2 == 0)
+			{
+				verticesList.Add(new Vector3(verticesList[verticesList.Count - 1].x, GetRandomFloat(randomRangeTop), 0));
+			}
+			else
+			{
+				verticesList.Add(new Vector3(verticesList[verticesList.Count - 1].x + GetRandomFloat(randomRangeBottom), 0, 0));
+			}
+		}
+	}
 
-		// for (int i = 0; i < 20; i++)
-		// {
-		// 	verticesList.Add(new Vector3(random.Next(), random.Next(), 0));
-		// }
+	private void GenerateTriangles()
+	{
+		for (int id = 2; id < verticesList.Count; id++)
+		{
+			if (id % 2 == 0)
+			{
+				trianglesList.Add(id - 2);
+				trianglesList.Add(id - 1);
+				trianglesList.Add(id);
+			}
+			else
+			{
+				trianglesList.Add(id - 1);
+				trianglesList.Add(id - 2);
+				trianglesList.Add(id);
+			}
+		}
+	}
 
-		// for (int i = 0; i < 20; i++)
-		// {
-		// 	trianglesList.Add(random.Next(0, verticesList.Count));
-		// 	trianglesList.Add(random.Next(0, verticesList.Count));
-		// 	trianglesList.Add(random.Next(0, verticesList.Count));
-		// }
-		
+	private float GetRandomFloat(Range range)
+	{
+		return (float) (random.NextDouble() * (range.max - range.min) + range.min);
 	}
 
 	private void UpdateShape()
@@ -52,7 +93,22 @@ public class Land : MonoBehaviour
 
 		mesh.vertices = verticesList.ToArray();
 		mesh.triangles = trianglesList.ToArray();
-
+		
+		verticesList.Clear();
+		trianglesList.Clear();
 		mesh.RecalculateNormals();
+	}
+}
+
+[Serializable]
+public struct Range
+{
+	public float min;
+	public float max;
+
+	public Range(float min, float max)
+	{
+		this.min = min;
+		this.max = max;
 	}
 }
